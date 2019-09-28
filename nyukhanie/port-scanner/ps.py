8e72888bd7bf96663788f16494d1c171d0cb0ad5
Angelo Giacco <giacco.angelo.s@gmail.com>
2019-09-28 18:19:04 +0100

    add port scanner threading and semaphore

--- nyukhanie/port-scanner/ps.py
+++ nyukhanie/port-scanner/ps.py
@@ -13,18 +13,25 @@ Scanning port 21
 """
 import optparse
 from socket import *
+from threading import *
+screenLock = Semaphore(value=1)
 
 def connScan(tgtHost,tgtPort):
     try:
         connSkt = socket(AF_INET, SOCK_STREAM)
         connSkt.connect((tgtHost, tgtPort))
-        connSkt.send('ViolentPython\r\n')
+        connSkt.send('My name is angelo\r\n')
         results = connSkt.recv(100)
+        screenLock.acquire()
         print '[+]%d/tcp open'% tgtPort
         print '[+] ' + str(results)
         connSkt.close()
     except:
+        screenLock.acquire()
         print('[-]%d/tcp closed'% tgtPort)
+    finally:
+        screenLock.release()
+        connSkt.close()
 
 def portScan(tgtHost, tgtPorts):
     try:
@@ -39,8 +46,8 @@ def portScan(tgtHost, tgtPorts):
         print '\n[+] Scan Results for: ' + tgtIP
     setdefaulttimeout(1)
     for tgtPort in tgtPorts:
-        print 'Scanning port ' + tgtPort
-        connScan(tgtHost, int(tgtPort))
+        t = Thread(target=connScan, args=(tgtHost, int(tgtPort)))
+        t.start()
 
 def main():
     parser = optparse.OptionParser('usage %prog -H'+\
